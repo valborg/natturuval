d171e5ad33fdecea562af39001724134cc1fe756
valborg <valgormur@gmail.com>
2024-05-23 15:34:44 +0000

    object tag for svg

--- src/App.js
+++ src/App.js
@@ -36,11 +36,12 @@ const App = () => {
                     </Nav.Item>
             </Nav>
         </Container>
-        <section id="main" className='main'>
+        <section id="main" className='App-main'>
       <Container>
         <Row>
             <Col className="col-sm-12 col-md-12 col-lg-6">
-                <img src={logoWithTagline}/>
+            <object id="mainLogo" data={logoWithTagline} type="image/svg+xml"></object>
+
                 <p className="someFont">We are proud to present our game Náttúruval that brings joy, laughter and science to anyone who plays it. 
                 The game is being funded via Gamefound and you can make your pledge there to secure a copy for you, yours and even an educational center if you are feeling charitable.</p>
             </Col>
@@ -96,7 +97,7 @@ const App = () => {
         <div className="firstHistroy">
         <Row>
         <Col className='col-sm-12 col-md-4 col-lg-5' >
-        <img src={logoWithTagline} alt="ITIS THE OLD"/>
+        <object id="historyLogo" data={logoWithTagline} type="image/svg+xml"></object>
         </Col>
         <Col className='col-sm-12 col-md-8 col-lg-7'>
             <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
@@ -117,7 +118,7 @@ const App = () => {
 "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
         </Col>
         <Col className='col-sm-12 col-md-4 col-lg-5'>
-        <img src={leaf} height="300" alt="ITIS THE OLD"/>
+        <object id="historyLeaf" data={leaf} type="image/svg+xml"></object>
         </Col>
 
         </Row>
