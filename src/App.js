701e43b79dd63756e21f6f1b2ba39438af7ec90b
valborg <valgormur@gmail.com>
2024-05-24 01:50:45 +0000

    responsive embedding

--- src/App.js
+++ src/App.js
@@ -16,10 +16,9 @@ const App = () => {
         <Container fluid >
             <Nav className='topnav'
                     activeKey="/home"
-                    onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
                     >
                     <Nav.Item>
-                        <Nav.Link href="#main">squarelogo/top/home</Nav.Link>
+                        <Nav.Link href="#main"><img src="/assets/Leaf.svg" width="40px" height="auto" alt="ITIS THE OLD"/></Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link href="#team">The team</Nav.Link>
@@ -28,7 +27,7 @@ const App = () => {
                         <Nav.Link href="#history">The story</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href='gamefound.com'>
+                        <Nav.Link href='https://gamefound.com/en/projects/search?term=n%C3%A1tt%C3%BAruval'>
                         Gamefound
                         </Nav.Link>
                     </Nav.Item>
@@ -36,27 +35,36 @@ const App = () => {
         </Container>
         <section id="main" className='main'>
       <Container>
-        <Row>
+        <Row className='row align-items-center'>
             <Col className="col-sm-12 col-md-12 col-lg-6">
-                <img src="/assets/NattLockupWithTagline.svg"/>
+                <img src="/assets/NattLockupWithTagline.svg" width="100%" height="auto"/>
                 <p className="someFont">We are proud to present our game Náttúruval that brings joy, laughter and science to anyone who plays it. 
                 The game is being funded via Gamefound and you can make your pledge there to secure a copy for you, yours and even an educational center if you are feeling charitable.</p>
             </Col>
             
             <Col className="col-sm-12 col-md-12 col-lg-6">
-                <iframe src="https://player.vimeo.com/video/949243604?h=94a7c4ea97" width="640" height="360" maxwidth="100%" maxheight="50%" frameborder="0" allow="fullscreen; picture-in-picture" allowfullscreen></iframe>
+                <div className='embed-responsive embed-responsive-16by9'>
+                    <iframe  className='embed-responsive-item' src="https://player.vimeo.com/video/949243604?h=94a7c4ea97"  height="180" width="320"  frameborder="0" allow="fullscreen; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </Col>
         </Row>
       </Container>
       </section>
       <section className="App-action">
-        <p><Button variant="outline-info">Check it out</Button> This is a limited time crowdfunding project so to get your hands on a copy you have to pledge or get a grown up to do it for you!</p>
+      <Row>
+        <Col>
+        <Button variant="outline-info">Check it out</Button>
+        </Col>
+        <Col>
+            <p>"This is a limited time crowdfunding project so to get your hands on a copy you have to pledge or get a grown up to do it for you!</p>
+        </Col>
+        </Row>
       </section>
-      <section className="App-about">
+      <section className="App-about" id="team">
         <h1>Meet the team!</h1>
-    <CardGroup>
+    <CardGroup className='card-group'>
         <Card className="col-sm-12 col-md-6 col-lg-3">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Boston_terrier_looking_upwards.png" height="100px" className="card-img-top" alt="boston terrier"/>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Boston_terrier_looking_upwards.png" width="100%" height="auto" className="card-img-top" alt="boston terrier"/>
             <div className="card-body">
                 <h5 className="card-title">Justin Richer</h5>
                 <p className="card-text">The top dog behind the project and when he's not out walking he's on the trail of securty breaches.</p>
@@ -64,23 +72,23 @@ const App = () => {
             </div>
         </Card>
         <Card className="col-sm-12 col-md-6 col-lg-3">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/2/28/Hairy_dragonfly_%28Brachytron_pratense%29_male_close_up.jpg"  className="card-img-top" alt="hairy dragonfly"/>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/28/Hairy_dragonfly_%28Brachytron_pratense%29_male_close_up.jpg"  width="100%" height="auto" className="card-img-top" alt="hairy dragonfly"/>
             <div  className="card-body">
             <h5  className="card-title">Kate Estrop</h5>
             <p  className="card-text">This bug has been buzzing around the look and feel of the project, and possibly even smell.</p>
             <Button href="https://kateestrop.com/" className="btn btn-warning">Go bug them</Button>
             </div>
         </Card>
-        <Card>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Randwick_bird_of_paradise_001.jpg"  className="card-img-top" alt="is it a bird?"/>
+        <Card className="col-sm-12 col-md-6 col-lg-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Randwick_bird_of_paradise_001.jpg" width="100%" height="auto" className="card-img-top" alt="is it a bird?"/>
             <div  className="card-body">
             <h5  className="card-title">Valborg Sturludóttir</h5>
             <p  className="card-text">Between fighting dragons and solving sudokus this bird mama tried to catch them all</p>
             <Button className='btn btn-info' href="https://github.com/valborg">Go on a nerdy adventure</Button>
             </div>
         </Card>  
-        <Card>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/43/Giant_Anteater_Santa_Barbara_Zoo_1.jpg"  className="card-img-top" alt="ant eater"/>
+        <Card className="col-sm-12 col-md-6 col-lg-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/4/43/Giant_Anteater_Santa_Barbara_Zoo_1.jpg"  width="100%" height="auto" className="card-img-top" alt="ant eater"/>
             <div  className="card-body">
             <h5  className="card-title">Rökkvi Þór</h5>
             <p  className="card-text">This young man's favorite scientist is David Attenborogh, and he can't wait to tell you facts about animals you've never heard of.</p>
@@ -89,12 +97,12 @@ const App = () => {
         </Card>
     </CardGroup>
     </section>
-    <section className='App-history'>
+    <section className='App-history' id="history">
         <h1>The story behind it</h1>
         <div className="firstHistroy">
         <Row>
         <Col className='col-sm-12 col-md-4 col-lg-5' >
-        <img src="/assets/NattLockupWithTagline.svg" alt="ITIS THE OLD"/>
+        <img src="/assets/NattLockupWithTagline.svg" width="100%" height="auto" alt="ITIS THE OLD"/>
         </Col>
         <Col className='col-sm-12 col-md-8 col-lg-7'>
             <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
@@ -115,7 +123,7 @@ const App = () => {
 "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
         </Col>
         <Col className='col-sm-12 col-md-4 col-lg-5'>
-        <img src="/assets/NattSquareLockup.png" height="300" alt="ITIS THE OLD"/>
+        <img src="/assets/NattSquareLockup.png" width="100%" height="auto" alt="ITIS THE OLD"/>
         </Col>
 
         </Row>
